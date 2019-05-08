77dbbc35eb5ce15facfa45d5a858e912471a7726
IvanLinhs <271065951@qq.com>
2019-05-08 20:26:42 +0800

    Update install_px2.sh

--- install_px2.sh
+++ install_px2.sh
@@ -8,16 +8,16 @@ fi
 sudo echo "10.42.0.28 tegra-a" >> /etc/hosts
 sudo echo "10.42.0.29 tegra-b" >> /etc/hosts
 
-sudo apt-get update
-sudo apt-get -y install apt-transport-https
+#sudo apt-get update
+#sudo apt-get -y install apt-transport-https
 
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
 sudo rm /etc/apt/sources.list
 
-sudo echo "deb https://mirrors.ustc.edu.cn/ubuntu-ports/ xenial main restricted universe multiverse" >> /etc/apt/sources.list
-sudo echo "deb https://mirrors.ustc.edu.cn/ubuntu-ports/ xenial-updates main restricted universe multiverse" >> /etc/apt/sources.list
-sudo echo "deb https://mirrors.ustc.edu.cn/ubuntu-ports/ xenial-backports main restricted universe multiverse" >> /etc/apt/sources.list
-sudo echo "deb https://mirrors.ustc.edu.cn/ubuntu-ports/ xenial-security main restricted universe multiverse" >> /etc/apt/sources.list
+sudo echo "deb http://mirrors.ustc.edu.cn/ubuntu-ports/ xenial main restricted universe multiverse" >> /etc/apt/sources.list
+sudo echo "deb http://mirrors.ustc.edu.cn/ubuntu-ports/ xenial-updates main restricted universe multiverse" >> /etc/apt/sources.list
+sudo echo "deb http://mirrors.ustc.edu.cn/ubuntu-ports/ xenial-backports main restricted universe multiverse" >> /etc/apt/sources.list
+sudo echo "deb http://mirrors.ustc.edu.cn/ubuntu-ports/ xenial-security main restricted universe multiverse" >> /etc/apt/sources.list
 sudo sh -c '. /etc/lsb-release && echo "deb https://mirrors.ustc.edu.cn/ros/ubuntu/ $DISTRIB_CODENAME main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
